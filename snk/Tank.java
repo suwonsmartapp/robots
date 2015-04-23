3afacf989fce2bc49b7857ca2172a819630c7e39
nam5 <sssn000@hamail.net>
2015-04-23 15:48:11 +0900

    robot

--- snk/Tank.java
+++ snk/Tank.java
@@ -10,25 +10,36 @@ import robocode.ScannedRobotEvent;
 public class Tank extends Robot {
 	@Override
 	public void run() {
-		while (true) {
-			turnRight(90);
-		}
-	}
-	
-	@Override
-	public void onScannedRobot(ScannedRobotEvent event) {
-		fire(1);
-	}
-	
-	@Override
-	public void onHitByBullet(HitByBulletEvent event) {
 		
-	}
-	
-	@Override
-	public void onHitWall(HitWallEvent event) {
 		
+    
+	
+	
+	 while (true) {
+	   turnGunRight(360); // 대포 오른쪽으로 360도 회전
+	   fire(getEnergy());
+		turnRight(90);
+		setAdjustGunForRobotTurn(true);
+		
+	 }
 	}
+	 
+	
+	 // 적을 발견되면
+	 public void onScannedRobot(ScannedRobotEvent e){
+	  // 거리가 짧으면 강하게 발사
+	  if (e.getDistance() < 20) {
+	   fire(3);
+	  } else
+	  { fire(1);
+	  }
+	 }
+	 // 우리가 포를 맞으면
+	 public void onHitByBullet(HitByBulletEvent e) {
+	  turnLeft(90);  // 탱크 왼쪽 90도로 꺽기
+	  ahead(100);   // 탱크 전진
+	 }
+	
 	
 	@Override
 	public void onMouseClicked(MouseEvent e) {
@@ -45,10 +56,7 @@ public class Tank extends Robot {
 		
 	}
 	
-	@Override
-	public void doNothing() {
-		
-	}
+
 	
 	
 
