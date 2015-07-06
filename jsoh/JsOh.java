ae5a567dac052d1e14fd20027da6c4e256c49732
Junsuk Oh <a811219@gmail.com>
2015-07-06 15:18:12 +0900

    따라가는 로봇

--- jsoh/JsOh.java
+++ jsoh/JsOh.java
@@ -1,28 +1,34 @@
 package jsoh;
 
 import java.awt.Color;
-import java.awt.Graphics2D;
-import java.util.Random;
 
 import robocode.AdvancedRobot;
 import robocode.HitRobotEvent;
-import robocode.HitWallEvent;
 import robocode.ScannedRobotEvent;
 import sampleteam.RobotColors;
 
 public class JsOh extends AdvancedRobot {
 	@Override
 	public void run() {
-		
+
 		init();
-		
+
 		while (true) {
-			setAhead(40000);
-			setTurnRight(90);
+			move();
 			execute();
 		}
 	}
-	
+
+	private void move() {
+		double distance = Math.random() * 40;
+		if (distance % 2 == 0) {
+			setAhead(distance);
+		} else {
+			setBack(distance);
+		}
+		setTurnRight(90);
+	}
+
 	private void init() {
 		RobotColors colors = new RobotColors();
 		colors.bodyColor = Color.BLACK;
@@ -30,34 +36,34 @@ public class JsOh extends AdvancedRobot {
 		colors.radarColor = Color.GREEN;
 		colors.bulletColor = Color.YELLOW;
 		colors.scanColor = Color.CYAN;
-		
-		setColors(colors.bodyColor,
-				colors.gunColor,
-				colors.radarColor,
-				colors.bulletColor,
-				colors.scanColor);
+
+		setColors(colors.bodyColor, colors.gunColor, colors.radarColor, colors.bulletColor, colors.scanColor);
 	}
-	
-	Random random = new Random();
-	
+
 	@Override
 	public void onScannedRobot(ScannedRobotEvent event) {
-		if (getEnergy() > 50) {
-			double power = random.nextDouble() * 3;
-			fire(power);
-		} else {
-			fire(1);
-		}
+		setTurnRight(event.getBearing());
+		setAhead(event.getDistance() + 5);
+
+		fire(3);
+
+		scan();
 	}
-	
+
 	@Override
 	public void onHitRobot(HitRobotEvent event) {
-		setAhead(100);
-		setTurnLeft(60);
-		execute();
+		if (event.getEnergy() > 16) {
+			fire(3);
+		} else if (event.getEnergy() > 10) {
+			fire(2);
+		} else if (event.getEnergy() > 4) {
+			fire(1);
+		} else if (event.getEnergy() > 2) {
+			fire(.5);
+		} else if (event.getEnergy() > .4) {
+			fire(.1);
+		}
+		setBack(40);
 	}
-	
-	@Override
-	public void onHitWall(HitWallEvent event) {
-	}
+
 }
